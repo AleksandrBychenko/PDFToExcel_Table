4d96ff91ff6970db143952370ab8c34a8bbb5033
AleksandrBychenko <bychenko.ad@phystech.edu>
2023-09-01 19:58:43 +0300

    end_app

--- P-T.py
+++ P-T.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import openpyxl
 #x = tabula.read_pdf('54564-54566.pdf', stream = True, multiple_tables = False, pages= page_num, relative_area = True,area=(29.4, 0, 49.3 +29.4 ,78))
-x = tabula.read_pdf('54564-54566.pdf', stream = True, multiple_tables = False, pages= 1, relative_area = True,area=(23, 0, 49.3 + 23 ,0 + 110))
+#x = tabula.read_pdf('54564-54566.pdf', stream = True, multiple_tables = False, pages= 1, relative_area = True,area=(23, 0, 49.3 + 23 ,0 + 110))
 
 #x = tabula.read_pdf('54564-54566.pdf', stream = True, multiple_tables = False, pages= page_num, relative_area = True,area=(23, 0, 29.4,0 + 110))
 #x = tabula.read_pdf('54564-54566.pdf', stream = True, multiple_tables = False, pages= page_num, relative_area = True)
@@ -13,7 +13,7 @@ x = tabula.read_pdf('54564-54566.pdf', stream = True, multiple_tables = False, p
 
 
 # Поиск места     
-y = tabula.read_pdf('54564-54566.pdf', stream = True, multiple_tables = False, pages = 1, relative_area = True,area=(3, 49, 11, 91))        
+#y = tabula.read_pdf('54564-54566.pdf', stream = True, multiple_tables = False, pages = 1, relative_area = True,area=(3, 49, 11, 91))        
 #CODe
 #y = tabula.read_pdf('54564-54566.pdf', stream = True, multiple_tables = False, pages = 1, relative_area = True,area=(15, 0, 20, 8))        
 #y = tabula.read_pdf('54564-54566.pdf', stream = True, multiple_tables = False, pages = 1, relative_area = True,area=(15, 8, 20, 23))
@@ -32,6 +32,8 @@ y = tabula.read_pdf('54564-54566.pdf', stream = True, multiple_tables = False, p
 #DISC = tabula.read_pdf('54564-54566.pdf', stream = True, multiple_tables = False, pages= 1, relative_area = True,area=(21, 74, 69, 78))
 #VALUE = tabula.read_pdf('54564-54566.pdf', stream = True, multiple_tables = False, pages= 1, relative_area = True,area=(21, 78, 69, 89))
 #CI = tabula.read_pdf('54564-54566.pdf', stream = True, multiple_tables = False, pages= 1, relative_area = True,area=(21, 88, 69, 94))
+y = tabula.read_pdf("54564-54566.pdf", stream = True, multiple_tables = False, pages= 8, relative_area = True,area=(21, 45, 69, 53))
+           
 check = np.array(y)
 print(check)
 
@@ -42,3 +44,58 @@ send = np.array(y)
 df = pd.DataFrame(send[0])
 
 df.to_excel('example.xlsx', index=False)
+
+--
+                                 # обработка этой строки на нужные слова !!!!!  - не учитывапется если нет sole
+                                my_list = send_y1[i][j][k].split('Country of origin:')
+                                if len(my_list) == 1: 
+                                    my_list = my_list[0].split('H. S')
+                                if len(my_list) > 1: 
+                                    my_list = my_list[1].split('H. S')
+                                
+                                if len(my_list) == 2:
+                                    worksheet['J' + str(row_index)] = my_list[0].replace(" ", "")
+                                    #oтделяем по :
+                                    hs = str(my_list[1].replace(":", "")) 
+                                    worksheet['K' + str(row_index)] = hs.replace(" ", "")
+                                    code = my_list[1].replace(" ", "")
+                                    code = code.replace(":", "")
+                                if len(my_list) > 2:
+                                    #!!!!!!!!!!!!!!!!
+                                    hs = str(my_list[0].replace(":", ""))
+                                    worksheet['J' + str(row_index)] = hs.replace(" ", "")
+                                    code = my_list[len(my_list) - 1].replace(" ", "")
+                                    code = code.replace(":", "")
+                                
+                                if len(my_list) < 2:
+                                    #!!!!!!!!!!!!!!!!
+                                    hs = str(my_list[0].replace(":", ""))
+                                    worksheet['J' + str(row_index)] = hs.replace(" ", "")
+                                    code = my_list[0].replace(" ", "")
+                                    code = code.replace(":", "")
+                                
+                                #print(code)
+                                '''
+                                if cdvig == last_row:
+                                    description = send_y1[i][j-2][k]
+                                    description_send = description.split(code)
+                                    
+                                    worksheet['H' + str(row_index)] = description_send[0]
+                                else:
+                                    
+                                    if row_index != last_row:
+                                        description = send_y1[i][j-2][k]
+                                        description_send = description.split(code)
+                                        worksheet['H' + str(row_index)] = description_send[0]
+                                '''
+                                description = send_y1[i][j-2][k]
+                                description_send = description.split(code)
+                                worksheet['H' + str(row_index)] = description_send[0]
+
+                                if first_ras and cdvig == last_row + 1:    
+                                    description = preded
+                                    description_send = description.split(code)
+                                    worksheet['H' + str(row_index)] = description_send[0] 
+                                    first_ras = False
+
+                                preded  =  send_y1[-1][-1][-1] 
--- Tinker.py
+++ Tinker.py
@@ -3,9 +3,9 @@ import tkinter as tk
 from tkinter import filedialog
 import PyPDF2
 import PDFToTEXT3 as pd3
-
+from PDFToTEXT3 import page_num
 def browse_file():
-    status_label.config(text="DO")
+    status_label.config(text="DO!")
     try:
         filepath = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
         #status_label.config(text="DO")
@@ -21,11 +21,11 @@ def browse_file():
                 # do something with the pdf contents
             '''
             #status_label.config(text=f"Done!\n Take a special Pdf file to convert to an excel file ")
-        status_label.config(text=f"Done!\n Take a special Pdf file to convert to an excel file ")
+        status_label.config(text=f"Done!\n\n Take a special Pdf file to convert to an excel file.\n\n Remove the processed excel files\n from the folder where they appeared!!! ")
 
 
     except Exception:
-        status_label.config(text="Ошибка")
+        status_label.config(text="Error!!!\n" + "\nMost likely the file format is not executed.")
         # здесь можно добавить дополнительный код для обработки ошибки, если это необходимо
     
 
@@ -41,7 +41,7 @@ root.configure(bg='green')
 browse_button = tk.Button(root, text="Take PDF file", command = browse_file,width= '10',height= '5' )
 browse_button.pack()
 
-status_label = tk.Label(root, text="Take a special Pdf file to convert to an excel file ",width='40', height= '10')
+status_label = tk.Label(root, text="Take a special Pdf file to convert to an excel file.\n\n Remove the processed excel files\n from the folder where they appeared\n if you have already used this application !!!",width='40', height= '10')
 status_label.pack()
 
 root.mainloop()
